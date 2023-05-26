2efb4e45cddbc20a4270e965549e605c532c015a
bartlst <bartlomiejstefanko@gmail.com>
2023-05-26 10:07:15 +0200

    Start of day 17: - how to create a method which will be interacting with another object

--- day-17/start/main.py
+++ day-17/start/main.py
@@ -1,10 +1,21 @@
 class User:
     def __init__(self, user_id, username):
+        print("NEW USER BEING CREATED...")
         self.id = user_id
         self.username = username
-        print("NEW USER BEING CREATED...")
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001", "admin")
+user_2 = User("002", "bartek")
 
-print(user_1.username)
+user_1.follow(user_2)
+print("User 1 following:", user_1.following)
+print("User 1 followers: ", user_1.followers)
+print("User 2 following:", user_2.following)
+print("User 2 followers: ", user_2.followers)
