b48102995f1e26d379ca187abbbebf0d70d0ddb9
bartlst <bartlomiejstefanko@gmail.com>
2023-05-28 13:48:01 +0200

    Day 18 start: - implementing new module "turtle" - checking module documentation - drawing simple square

---
+++ day-18/day-18-start/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+adam = Turtle()
+
+screen = Screen()
+adam.forward(50)
+adam.left(90)
+adam.forward(50)
+adam.left(90)
+adam.forward(50)
+adam.left(90)
+adam.forward(50)
+adam.left(90)
+screen.exitonclick()
