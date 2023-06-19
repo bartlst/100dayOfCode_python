97780dc779f16da902fdebee8a74d5d9ddd93d4d
bartlst <bartlomiejstefanko@gmail.com>
2023-06-19 18:33:46 +0200

    Day 26 finished. Learning about list and dict comprehension by building a simple project which is translating word's letters into a NATO Alphabet

--- Day_25/project/main.py
+++ Day_25/project/main.py
@@ -11,6 +11,12 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 correct_answers = []
+remaining_states = []
+data_dict={
+    'state' : [],
+    'x':[],
+    'y':[]
+}
 
 def show_sate(state_data):
     t = turtle.Turtle()
@@ -27,15 +33,28 @@ game_running = True
 while game_running:
     time.sleep(1)
     user_guest = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state name?")
-
+    user_guest = user_guest.title()
     if user_guest in data['state'].unique() and user_guest not in correct_answers:
         score += 1
         correct_answers.append(user_guest)
         state = (data[data['state'] == user_guest])
         show_sate(state)
-    else:
-        print("No")
+
+    if user_guest == "Exit":
+        game_running = False
     if score == 50:
         game_running = False
 
+remaining_states = [state_name for state_name in data['state'].to_list() if state_name not in correct_answers]
+
+
+for state_name in remaining_states:
+    data_dict['state'].append(data[data['state'] == state_name].state.item())
+    data_dict['x'].append(data[data['state'] == state_name].x.item())
+    data_dict['y'].append(data[data['state'] == state_name].y.item())
+
+
+final_data = pandas.DataFrame(data_dict)
+final_data.to_csv('new_csv.csv')
+print(data_dict)
 screen.exitonclick()
---
+++ Day_26/start/main.py
@@ -0,0 +1,8 @@
+import random
+
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+
+student_score = {name:random.randint(1,100) for name in names}
+
+print(student_score)
+
